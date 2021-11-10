93eca318214bfdd6372bdc4489d9ade3a24346bc
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-09 23:43:01 -0500

    ls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    buildTodos(e.target.newtaskdescription.value)
+    form.reset()
+  })
+})
+
+function buildTodos(todos){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${todos} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
